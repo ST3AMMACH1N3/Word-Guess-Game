dbf764953ea10660cfe4801dbb63633e8e3e8387
Ben Houston <BenjaminAHouston@gmail.com>
2018-09-09 14:28:33 -0400

    Added a progress variable to track the players progress on the current word, and wrote a function to reset the players progress.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ window.onload = function() {
     var game = {
         wins: 0,
         word: "",
+        progress: [],
         wordList: ["html", "css", "javascript", "array", "function", "method", "variable", "string", "boolean", "object", "loop", "conditional", "agile", "bootstrap", "react"],
         beginElem: document.getElementById("begin-label"),
         winElem: document.getElementById("wins-output"),
@@ -11,6 +12,14 @@ window.onload = function() {
 
         pickWord: function() {
             this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+        },
+
+        reset: function() {
+            this.pickWord();
+            this.progress = [];
+            for(var i = 0; i < this.word.length; i++) {
+                this.progress.push("_");
+            }
         }
     }
 }
